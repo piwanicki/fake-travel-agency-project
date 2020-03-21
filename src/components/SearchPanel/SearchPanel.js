21c59b0077e96e523e8db3d821678e48152df7a7
piwanicki <pawel.iwanicki92@gmail.com>
2020-03-21 14:17:05 +0100

    change searchbox styling

--- src/components/SearchPanel/SearchPanel.js
+++ src/components/SearchPanel/SearchPanel.js
@@ -1,25 +1,45 @@
-import React, { Component } from 'react';
-import classes from './SearchPanel.module.css';
+import React, { Component } from "react";
+import classes from "./SearchPanel.module.css";
 
 class SearchPanel extends Component {
   render() {
     return (
       <div className={classes.SearchContainer}>
+        <div className={classes.SearchBox}>
+          <p style={{ fontSize: "2.2em", margin: "1em" }}>
+            Find your dreamy vacation!
+          </p>
           <div className={classes.SearchPanel}>
-          <p>Find your dreamy vacation!</p>
-            <select>
-              <option> Fly </option>
-              <option> Bus </option>
-              <option> Own car </option>
-            </select>
-            <input type='text' placeholder='Direction' />
-            <input type='date' placeholder='From' /> 
-            <input type='date' placeholder='To' /> 
-            <button type='button'> Search </button>
+            <div>
+              <p>Transport</p>
+              <select>
+                <option> Fly </option>
+                <option> Bus </option>
+                <option> Own car </option>
+              </select>
+            </div>
+
+            <div>
+              <p>Where</p>
+              <input type="text" placeholder="Direction" />
+            </div>
+
+            <div>
+              <p>Since when</p>
+              <input type="date" placeholder="From" />
+            </div>
+
+            <div>
+              <p>Until when</p>
+              <input type="date" placeholder="To" />
+            </div>
+
+            <button type="button"> Search </button>
           </div>
+        </div>
       </div>
-    )
+    );
   }
-};
+}
 
 export default SearchPanel;
