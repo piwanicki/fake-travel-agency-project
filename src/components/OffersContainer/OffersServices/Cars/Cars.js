e6bad04b9cf8f82fa00e3339acdc68b570ab7b2c
Pawel Iwanicki <pawel.iwanicki92@gmail.com>
2020-04-25 17:59:39 +0200

    add simple alphabetical sorting

--- src/components/OffersContainer/OffersServices/Cars/Cars.js
+++ src/components/OffersContainer/OffersServices/Cars/Cars.js
@@ -1,53 +1,77 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./Cars.module.scss";
 import CarOffer from "./CarOffer/CarOffer";
 import CarsFilter from "./CarOffer/CarsFilter/CarsFilter";
 import CarsOffers from "./CarsOffers";
 
-const Cars = (props) => {
-  const vehicleBrands = Object.keys(CarsOffers);
-  const brandsModels = new Map();
+class Cars extends Component {
+  state = {
+    allModels: [],
+    vehicleBrands: [],
+  };
 
-  let allModels = [];
-  vehicleBrands.forEach((brand) => {
-    brandsModels.set(brand, CarsOffers[brand]);
-  });
-  brandsModels.forEach((brandsModels, brand) => {
-    const models = Object.keys(brandsModels.models);
-    const brandLogo = brandsModels.logo;
-    allModels.push(
-      models.map((model, index) => (
-        <CarOffer
-          key={index}
-          brand={brand}
-          model={brandsModels.models[model]}
-          modelName={model}
-          logo={brandLogo}
-        />
-      ))
-    );
-  });
+  componentDidMount = () => {
+    const vehicleBrands = Object.keys(CarsOffers);
+    const brandsModels = new Map();
+    let allModels = this.state.allModels;
 
-  const filterList = (e) => {
+    vehicleBrands.forEach((brand) => {
+      brandsModels.set(brand, CarsOffers[brand]);
+    });
+    brandsModels.forEach((brandsModels, brand) => {
+      const models = Object.keys(brandsModels.models);
+      const brandLogo = brandsModels.logo;
+      allModels = allModels.concat(
+        models.map((model) => {
+          return (
+            <CarOffer
+              key={model}
+              brand={brand}
+              model={brandsModels.models[model]}
+              modelName={model}
+              logo={brandLogo}
+            />
+          );
+        })
+      );
+    });
+    this.setState({
+      allModels: allModels,
+      vehicleBrands: vehicleBrands,
+    });
+  };
+
+  filterList = (e) => {
     const dataSetVal = Object.values(e.target.dataset);
     const method = dataSetVal[0];
     const methodValue = e.target.value;
+    let outputList = [];
+    const allModels = [...this.state.allModels];
+    console.log(allModels);
 
     if (method === "sorting") {
       console.log(`sorting method by ${methodValue}`);
-
-      console.log(allModels)
+      outputList = allModels.sort((a, b) => {
+        return a.key < b.key ? -1 : a.key > b.key ? 1 : 0;
+      });
+      console.log(outputList);
+      this.setState({ allModels: outputList });
     } else {
       console.log(`filter method by ${methodValue}`);
     }
   };
 
-  return (
-    <div className={classes.Cars}>
-      <CarsFilter brands={vehicleBrands} filterList={filterList} />
-      {allModels}
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className={classes.Cars}>
+        <CarsFilter
+          brands={this.state.vehicleBrands}
+          filterList={this.filterList}
+        />
+        {this.state.allModels}
+      </div>
+    );
+  }
+}
 
 export default Cars;
