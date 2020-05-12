f11c12d8ce46707ad34a8d87a8079f9d11172d97
piwanicki <pawel.iwanicki92@gmail.com>
2020-05-12 12:35:45 +0200

    add form validation, add rent Agreement simple component

--- src/components/OffersContainer/OffersServices/Cars/CarOffer/CarOfferDetails/CarRentForm/CarRentForm.js
+++ src/components/OffersContainer/OffersServices/Cars/CarOffer/CarOfferDetails/CarRentForm/CarRentForm.js
@@ -1,77 +1,118 @@
-import React from "react";
+import React, {useState} from "react";
 import classes from "./CarRentForm.module.scss";
 import CustomSelect from "../../../../../../../UI/CustomSelect/CustomSelect";
+import RentAgreement from "./RentAgreement/RentAgreement";
 
 const CarRentForm = (props) => {
+  const [showRentAgreement, showHandler] = useState(false);
+  const [acceptAgreement, acceptHandler] = useState(false);
+
+  const openRentAgreementHandler = () => {
+    showHandler(!showRentAgreement);
+  };
+
+  const acceptRentAgreementHandler = () => {
+    acceptHandler(!acceptAgreement);
+  };
+
+  const rentHandler = (event) => {
+    event.preventDefault();
+    console.log(`rentHandler`);
+  };
+
   return (
-    <div className={classes.CarRentForm}>
-      <p>
-        Please fill the rent form and accept rules to complete the process...
-      </p>
+    <form onSubmit={rentHandler}>
+      <div className={classes.CarRentForm}>
+        <p style={{marginLeft: "1em"}}>
+          Please fill the rent form and accept rules to complete the process...
+        </p>
 
-      <div className={classes.PersonalData}>
-        <p>Personal data </p>
-        <input type="text" placeholder="Name" />
-        <input type="text" placeholder="Surname" />
-      </div>
-
-      <div className={classes.TermData}>
-        <p>Term </p>
-        <span>
-          From : <br />
-          <input type="date" />
-        </span>
-        <span>
-          To : <br />
-          <input type="date" />
-        </span>
-      </div>
-
-      <div className={classes.TermData}>
-        <p>Hours </p>
-        <span>
-          Start time : <br />
-          <input type="time" placeholder="Start" />
-        </span>
-        <span>
-          End time : <br />
-          <input type="time" placeholder="End" />
-        </span>
-      </div>
-
-      <div className={classes.RentLocation}>
-        <CustomSelect description="Location" descDetails="Pick your city :">
-          <option>Somewhere1</option>
-          <option>Somewhere2</option>
-          <option>Somewhere3</option>
-          <option>Somewhere4</option>
-        </CustomSelect>
-
-        <div style={{textAlign: "left", width: "210px", marginTop: "1em"}}>
-          <div
-            style={{
-              display: "inline-block",
-              width: "130px",
-              textAlign: "center",
-              transform: "translateY(25%)",
-            }}
-          >
-            Period of having a driving license
-          </div>
-          <input type="number" min="0" style={{width: "70px"}} />
+        <div className={classes.PersonalData}>
+          <p>Personal data </p>
+          <input type="text" placeholder="Name" required />
+          <input type="text" placeholder="Surname" required />
         </div>
-      </div>
 
-      <div className={classes.AcceptBtn}>
-        <span>
-          <input type="checkbox" />
-          Check here to indicate that you have read and agree to the terms of
-          the &nbsp;
-          <a href="#CarAgreement">Car Renting Agreement</a>.
-        </span>
-        <button type="submit">Rent</button>
+        <div className={classes.TermData}>
+          <p>Term </p>
+          <span>
+            From : <br />
+            <input type="date" required />
+          </span>
+          <span>
+            To : <br />
+            <input type="date" required />
+          </span>
+        </div>
+
+        <div className={classes.TermData}>
+          <p>Hours </p>
+          <span>
+            Start time : <br />
+            <input type="time" placeholder="Start" required />
+          </span>
+          <span>
+            End time : <br />
+            <input type="time" placeholder="End" required />
+          </span>
+        </div>
+
+        <div className={classes.RentLocation}>
+          <CustomSelect description="Location" descDetails="Pick your city :">
+            <option>Somewhere1</option>
+            <option>Somewhere2</option>
+            <option>Somewhere3</option>
+            <option>Somewhere4</option>
+          </CustomSelect>
+
+          <div style={{textAlign: "left", width: "210px", marginTop: "1em"}}>
+            <div
+              style={{
+                display: "inline-block",
+                width: "130px",
+                textAlign: "center",
+                transform: "translateY(25%)",
+              }}
+            >
+              Period of having a driving license
+            </div>
+            <input type="number" min="0" style={{width: "70px"}} required />
+          </div>
+        </div>
+
+        <div className={classes.AcceptBtn}>
+          <span>
+            <input
+              type="checkbox"
+              onChange={acceptRentAgreementHandler}
+              required
+              checked={acceptAgreement}
+            />
+            * Check here to indicate that you have read and agree to the terms
+            of the &nbsp;
+            <span
+              style={{
+                color: "blue",
+                textDecoration: "underline",
+                cursor: "pointer",
+              }}
+              onClick={openRentAgreementHandler}
+            >
+              Car Renting Agreement
+            </span>
+            .
+          </span>
+          <button type="submit">Rent</button>
+        </div>
+
+        {showRentAgreement ? (
+          <RentAgreement
+            showRentAgreement={openRentAgreementHandler}
+            acceptRentAgreement={acceptRentAgreementHandler}
+          />
+        ) : null}
       </div>
-    </div>
+    </form>
   );
 };
 
---
+++ src/components/OffersContainer/OffersServices/Cars/CarOffer/CarOfferDetails/CarRentForm/RentAgreement/RentAgreement.js
@@ -0,0 +1,73 @@
+import React from "react";
+import classes from "./RentAgreement.module.scss";
+
+const RentAgreement = (props) => {
+  return (
+    <div className={classes.RentAgreement}>
+      <h3>Rent Agreement</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras volutpat
+        vel tellus at venenatis. Mauris mattis odio sapien, vel dictum eros
+        lacinia nec. Nunc enim enim, porttitor et augue in, sollicitudin
+        accumsan dui. Pellentesque efficitur mi purus, in luctus mauris
+        ullamcorper id. Mauris efficitur libero mauris, et feugiat tortor
+        condimentum id. Nunc tristique et mauris non finibus. In eros velit,
+        bibendum nec orci quis, auctor pharetra nisl. Aliquam dignissim orci a
+        vestibulum pharetra. Curabitur varius porta dictum. Maecenas ultricies
+        mauris nisl, nec elementum nisi dapibus ut. Pellentesque vulputate diam
+        ligula, at tempor sapien aliquam sit amet. Etiam volutpat libero odio,
+        quis efficitur est maximus ut. In eu sapien vitae elit gravida
+        consequat. Curabitur aliquet eu diam nec elementum. Etiam nec nulla eu
+        sapien rutrum egestas. Nunc pretium mauris sed fermentum fermentum. Nunc
+        mattis nibh ut leo sollicitudin, sollicitudin euismod lectus faucibus.
+        Nunc vitae interdum elit. Proin feugiat orci ex. Praesent elementum
+        risus eget justo pharetra tempor.
+      </p>
+
+      <p>
+        Quisque finibus eros a mi fermentum, eget eleifend lorem viverra.
+        Phasellus efficitur faucibus felis, id maximus eros ullamcorper eu.
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+        cubilia curae; Aenean sed maximus leo. Vestibulum placerat turpis nec
+        tortor vestibulum porttitor. Nulla quis diam elementum, blandit neque
+        sed, rhoncus enim. Proin enim leo, scelerisque id nisl consequat,
+        convallis pharetra metus. Donec eros eros, pretium ac ullamcorper id,
+        consectetur vitae risus. Maecenas posuere scelerisque posuere. Nullam
+        lacinia et urna a efficitur. Pellentesque ullamcorper mi id lorem
+        imperdiet, vitae tempor orci rutrum. Ut blandit ex sit amet arcu
+        tincidunt faucibus eget non nisi. Proin in justo eu massa tempus
+        pulvinar sed et dolor. Integer ex nibh, fringilla vel elit vitae,
+        dapibus fringilla lacus. Donec viverra consectetur enim, pharetra
+        maximus risus iaculis vel. Vestibulum euismod lectus vitae orci mattis
+        pharetra. Maecenas tortor massa, commodo in odio nec, elementum feugiat
+        erat. In condimentum tincidunt hendrerit. Duis cursus lobortis odio, sit
+        amet egestas orci ultricies vel. Nam eget sem augue.
+      </p>
+
+      <p>
+        Integer scelerisque ligula et ante dictum, a venenatis nisi dignissim.
+        Proin imperdiet dolor ac lorem tempor porttitor. Phasellus efficitur sed
+        neque at mattis. Vestibulum sit amet lectus tincidunt, lacinia eros
+        quis, sollicitudin turpis. Nulla vel tortor risus. Ut at augue congue,
+        congue ex nec, volutpat mi. Praesent et ullamcorper augue, vel aliquam
+        neque. Cras lorem enim, finibus et tempor eu, dapibus et augue. Sed
+        tempor libero non leo sodales congue. Mauris quis enim mauris. In hac
+        habitasse platea dictumst. Proin fermentum augue nisi, sed viverra nisl
+        bibendum id. Mauris porta enim quis magna molestie varius. Donec varius
+        odio diam, ac facilisis tortor convallis a. Aliquam erat volutpat. Duis
+        sed pulvinar dolor.
+      </p>
+
+      <span>
+        I have read and accept rent agreement.
+        <input type="checkbox" onChange={props.acceptRentAgreement} />
+      </span>
+
+      <span onClick={props.showRentAgreement} className={classes.RejectBtn}>
+        Close
+      </span>
+    </div>
+  );
+};
+
+export default RentAgreement;
